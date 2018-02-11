f80d9b2ca91e1d49868ca66b175769320d6817a9
a4arjun <arjun.venugopal@hotmail.com>
2018-02-11 18:33:04 +0530

    Update header.php

--- header.php
+++ header.php
@@ -59,20 +59,16 @@
                     </a>
                 </li>
                 <li>
-                    <a rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank" class="btn btn-white btn-simple btn-just-icon">
+                    <a rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/a2arjun" target="_blank" class="btn btn-white btn-simple btn-just-icon">
                         <i class="fa fa-twitter"></i>
                     </a>
                 </li>
                 <li>
-                    <a rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank" class="btn btn-white btn-simple btn-just-icon">
+                    <a rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/arjun.peariya" target="_blank" class="btn btn-white btn-simple btn-just-icon">
                         <i class="fa fa-facebook-square"></i>
                     </a>
                 </li>
-                <li>
-                    <a rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank" class="btn btn-white btn-simple btn-just-icon">
-                        <i class="fa fa-instagram"></i>
-                    </a>
-                </li>
+
 
             </ul>
         </div>
